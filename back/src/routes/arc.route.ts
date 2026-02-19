2813b3e887b9172bda698e233f8972ae28cf93bf
Rohan <baviere.ruddy62@email.com>
2026-02-19 11:35:44 +0100

    :sparkles: feat/back-model : add routes arc,organisation and equipage and update model

---
+++ back/src/routes/arc.route.ts
@@ -0,0 +1,7 @@
+import express, { Request, Response } from "express";
+import db from "@/lib/db";
+
+const router: express.Router = express.Router();
+
+
+export default router;
---
+++ back/src/routes/equipage.route.ts
@@ -0,0 +1,6 @@
+import express, { Request, Response } from "express";
+import db from "@/lib/db";
+
+const router: express.Router = express.Router();
+
+export default router;
