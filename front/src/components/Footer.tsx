22bdefffb3d9bbedf6d5f7059e161bd51715523a
Ruddy <baviere.ruddy62@gmail.com>
2026-04-07 11:59:49 +0200

    :zap: feat/develop: update responsive for footer

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className="bg-[#1D293D] px-10 text-white flex flex-col items-center py-10 gap-4 max-md:flex-row max-md:justify-center max-md:gap-9">
       <div className="flex flex-col items-center max-md: w-1/2">
       <img src={LogoFooter} alt="Logo du site" className="w-50"/>
-      <p className="max-md:text-center">Laugh Tale Tavern — Une encyclopédie One Piece créée par des fans</p>
+      <p className="max-lg:text-center">Laugh Tale Tavern — Une encyclopédie One Piece créée par des fans</p>
       </div>
       <nav className="max-md:w-1/2 max-sm:hidden">
         <ul className="flex gap-8 text-sm text-center items-center max-md:gap-3 max-md:flex-col max-md:text-left">
