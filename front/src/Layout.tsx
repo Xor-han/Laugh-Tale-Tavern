ae01726da7e41c202bfb4c2dd0fc1170dae96c4e
Xor-han <baviere.ruddy62@gmail.com>
2026-04-07 07:49:34 +0200

    :zap: feat/develop: update layout

--- front/src/Layout.tsx
+++ front/src/Layout.tsx
@@ -6,10 +6,18 @@ import { authClient } from "./lib/auth-client";
 export const Layout = () =>{
     const { data: session } = authClient.useSession();
     return(
-        <main>
-            <Navbar userId={session?.user.id} userImage={session?.user.image} userName={session?.user.name} session={session ? true : false}/>
-            <Outlet/>
-            <Footer/>
-        </main>
+      <div className="flex flex-col min-h-screen">
+      <Navbar 
+        userId={session?.user.id} 
+        userImage={session?.user.image} 
+        userName={session?.user.name} 
+        session={session ? true : false}
+      />
+      <main className="grow bg-slate-400">
+        <Outlet />
+      </main>
+      
+      <Footer />
+    </div>
     )
 }
